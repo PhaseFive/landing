bc33e76e6616d148063fde2537416bd4ba9f9376
TrevorJamesH <jetsamrover@gmail.com>
2017-10-13 10:52:16 -0700

    Adds mobile responsiveness, some content

---
+++ src/components/About/index.js
@@ -0,0 +1,9 @@
+const landingText = (
+  ``
+)
+
+const aboutText = (
+  `We are a team of expert web developers coming from diverse backgrounds who
+  are passionate about building a better web experience and delivering quality
+  code to our clients`
+)
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,6 +5,83 @@ import Grid from 'material-ui/Grid';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Hidden from 'material-ui/Hidden';
+import Drawer from 'material-ui/Drawer';
+import MenuIcon from 'material-ui-icons/Menu';
+import IconButton from 'material-ui/IconButton';
+
+class Navbar extends Component {
+  constructor(){
+    super()
+    this.state = {
+      mobileOpen: false
+    }
+
+    this.handleDrawerToggle = this.handleDrawerToggle.bind(this)
+    this.handleDrawerClose = this.handleDrawerClose.bind(this)
+  }
+
+  handleDrawerToggle(){
+    this.setState({mobileOpen: !this.state.mobileOpen})
+  }
+
+  handleDrawerClose(){
+    this.setState({mobileOpen: false})
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    const navButtons = navLinks.map((navLink, index) => {
+      return (
+        <Button type="button" color="inherit">
+          <a className={classes.navLink} href={navLink.href}>
+            {navLink.title}
+          </a>
+        </Button>
+      )
+    })
+
+    return (
+      <AppBar position="fixed">
+        <Toolbar className={classes.root}>
+          <Hidden mdUp>
+            <IconButton
+              color="contrast"
+              aria-label="open drawer"
+              onClick={this.handleDrawerToggle}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              color='accent'
+              type="temporary"
+              open={this.state.mobileOpen}
+              onRequestClose={this.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {navButtons}
+            </Drawer>
+          </Hidden>
+
+          <Typography type="title" color="inherit" className={classes.flex}>
+            Phase 5
+          </Typography>
+
+          <Hidden smDown>
+            {navButtons}
+          </Hidden>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const styles = theme => ({
   root: {
@@ -25,40 +102,18 @@ const styles = theme => ({
     '&:active': {
       color: 'red',
     },
-    paddingLeft: 20,
   },
 })
 
-class Navbar extends Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Grid item xs={12}>
-        <AppBar position="fixed">
-          <Toolbar className={classes.root}>
-            <Typography type="title" color="inherit" className={classes.flex}>
-              Phase 5
-            </Typography>
-            <Typography type="button" color="inherit">
-              <a className={classes.navLink} href="#about">
-                About
-              </a>
-            </Typography>
-            <Typography type="button" color="inherit">
-              <a className={classes.navLink} href="#contact">
-                Contact
-              </a>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Grid>
-    );
-  }
-}
-
-Navbar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+const navLinks = [
+  {
+    title: 'About',
+    href: '#about'
+  },
+  {
+    title: 'Contact',
+    href: '#contact'
+  },
+]
 
 export default withStyles(styles)(Navbar);
